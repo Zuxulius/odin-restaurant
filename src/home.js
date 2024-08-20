c53d89071564116e07d3aa9b6aec510c617f3ffc
Axel <70969800+Zuxulius@users.noreply.github.com>
2024-08-20 20:35:08 +0200

    styling

--- src/home.js
+++ src/home.js
@@ -1,9 +1,7 @@
 import homeImage from "../grasshoppers.jpg";
 
-const image = document.createElement("img");
-image.src = homeImage;
-image.alt = "An image of plated grasshoppers."
-image.classList.add("homeImage");
+const div = document.createElement("div");
+div.classList.add("wrapper")
 
 const h1 = document.createElement("h1");
 h1.innerText = "Hop in the Grass";
@@ -16,14 +14,14 @@ p.innerText =
 `Hop in the Grass is a restaurant with one Michelin star.
 We serve only the finest grasshoppers from exotic ends of the earth,
 prepared in the style of traditional French cuisine.
-Indulge in the novel tastes of grasshoppers with cosy, homey flavour.
-Transport yourself to the French countryside and "Hop in the Grass"!`
+Indulge in the novel tastes of grasshoppers with a cosy, homey flavour.
+Let yourself be transported to the French countryside and "Hop in the Grass"!`
 
 const content = document.getElementById("content");
 
 export default function init() {
-    content.appendChild(image);
-    content.appendChild(h1);
-    content.appendChild(h3);
-    content.appendChild(p);
+    content.appendChild(div)
+    div.appendChild(h1);
+    div.appendChild(h3);
+    div.appendChild(p);
 };
