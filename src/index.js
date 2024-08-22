139f252aad067825bb8762eecab12093161945db
Axel <70969800+Zuxulius@users.noreply.github.com>
2024-08-22 16:55:29 +0200

    Add tab focus logic and styling

--- src/index.js
+++ src/index.js
@@ -11,4 +11,24 @@ homeBtn.addEventListener('click', initHome);
 menuBtn.addEventListener('click', initMenu);
 aboutBtn.addEventListener('click', initAbout);
 
+document.querySelector('header').addEventListener('click', (e) => focusTab(e));
+
+function focusTab(e) {
+    if (e.target.tagName === "BUTTON") {
+        const tabs = document.querySelectorAll('header button');
+        tabs.forEach((tab) => tab.classList.remove('focus'));
+
+        switch (e.target) {
+            case homeBtn:
+                homeBtn.classList.toggle('focus');
+                break;
+            case menuBtn:
+                menuBtn.classList.toggle('focus');
+                break;
+            case aboutBtn:
+                aboutBtn.classList.toggle('focus');
+        }
+    }
+}
+
 initHome();
